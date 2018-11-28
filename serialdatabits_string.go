4beccb2b525ed374dda8009660e02c03a8b65e13
Craig Hesling <craig@hesling.com>
2018-11-28 00:07:47 -0500

    Updated stringer for SerialDataBits

--- serialdatabits_string.go
+++ serialdatabits_string.go
@@ -9,8 +9,9 @@ const _SerialDataBits_name = "SerialDataBits7BitsSerialDataBits8Bits"
 var _SerialDataBits_index = [...]uint8{0, 19, 38}
 
 func (i SerialDataBits) String() string {
+	i -= 1
 	if i >= SerialDataBits(len(_SerialDataBits_index)-1) {
-		return "SerialDataBits(" + strconv.FormatInt(int64(i), 10) + ")"
+		return "SerialDataBits(" + strconv.FormatInt(int64(i+1), 10) + ")"
 	}
 	return _SerialDataBits_name[_SerialDataBits_index[i]:_SerialDataBits_index[i+1]]
 }
