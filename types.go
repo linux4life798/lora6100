13e920e4fe1c09559d283d07e7d1c58277839fd0
Craig Hesling <craig@hesling.com>
2018-11-27 23:42:21 -0500

    Added enumerated baud rate and data bits

--- types.go
+++ types.go
@@ -35,20 +35,70 @@ const (
 	CmdSetParameters  Cmd = 0x03
 )
 
+type SerialBaudRate byte
+
+const (
+	SerialBaudRate1200 SerialBaudRate = iota
+	SerialBaudRate2400
+	SerialBaudRate4800
+	SerialBaudRate9600
+	SerialBaudRate14400
+	SerialBaudRate19200
+	SerialBaudRate38400
+	SerialBaudRate57600
+	SerialBaudRate76800
+	SerialBaudRate115200
+	SerialBaudRateUnknown
+)
+
+func (b *SerialBaudRate) FromSpeed(baud int) {
+	switch baud {
+	case 1200:
+		*b = SerialBaudRate1200
+	case 2400:
+		*b = SerialBaudRate2400
+	case 4800:
+		*b = SerialBaudRate4800
+	case 9600:
+		*b = SerialBaudRate9600
+	case 14400:
+		*b = SerialBaudRate14400
+	case 19200:
+		*b = SerialBaudRate19200
+	case 38400:
+		*b = SerialBaudRate38400
+	case 57600:
+		*b = SerialBaudRate57600
+	case 76800:
+		*b = SerialBaudRate76800
+	case 115200:
+		*b = SerialBaudRate115200
+	default:
+		*b = SerialBaudRateUnknown
+	}
+}
+
+type SerialDataBits byte
+
+const (
+	SerialDataBits7Bits SerialDataBits = iota
+	SerialDataBits8Bits
+)
+
 // 31 bytes total
 type Parameters struct {
 	RFChannel      byte
 	RFFreq         byte
-	RFDataRate     byte     // 0-9
-	TXPower        byte     // 0-7
-	SerialBaud     byte     // 0-9
-	SerialDataBits byte     // 1 (7bit) or 2(8bits)
-	SerialStopBits byte     // 1 (1bit) or 2 (2bits)
-	SerialParity   byte     // 1 (none), 2 (odd), 3 (even)
-	NetID          uint32   // 0x00000000 - 0xFFFFFFFF
-	NodeID         uint16   // 0x0000 - 0xFFFF
-	AESKeySetting  byte     // 0 (default-does not use defined AES key) or 1 (user-defined AES key)
-	AESKey         [16]byte // AES 128 key
+	RFDataRate     byte           // 0-9
+	TXPower        byte           // 0-7
+	SerialBaud     SerialBaudRate // 0-9
+	SerialDataBits SerialDataBits // 1 (7bit) or 2(8bits)
+	SerialStopBits byte           // 1 (1bit) or 2 (2bits)
+	SerialParity   byte           // 1 (none), 2 (odd), 3 (even)
+	NetID          uint32         // 0x00000000 - 0xFFFFFFFF
+	NodeID         uint16         // 0x0000 - 0xFFFF
+	AESKeySetting  byte           // 0 (default-does not use defined AES key) or 1 (user-defined AES key)
+	AESKey         [16]byte       // AES 128 key
 }
 
 func (p *Parameters) ReadFrom(in io.Reader) (int64, error) {
